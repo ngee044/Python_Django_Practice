c38e90e527c37aa660bdaec3f71cf4445fdd3fea
ngee044 <mgee044@naver.com>
2021-06-29 17:43:02 +0900

    기본 사용 법 튜토리얼 사항을 코딩 해봄

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    # Question Class를 참조하겠다 ForeignKey을 잘 알아보자 on_delete는 Question 객체가 지워지면 나도 같이 지워지겠다.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
